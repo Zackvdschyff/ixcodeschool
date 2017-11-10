c151e2ddb115fd4d008f80c4dd2464e59c4dfe8a
Zack <zack@ixperience.co.za>
2017-11-10 11:46:25 +0200

    Quizes complete

--- 6.rb
+++ 6.rb
@@ -1,8 +1,20 @@
-def max_number(arr)
-  #end
-  #puts max_number[0]
-  max_number.max_by do |x|
-    arr.x
+def max_number(numbers)
+max_value = 0
+  for x in numbers
+    if x > max_value
+      max_value = x
+    end
+  end
+return max_value
 end
 
-puts max_number([5, 0, 3, 8, 2])
+max_value = max_number([5, 0, 3, 8, 2])
+puts max_value
+
+
+
+
+#Write a function that returns the largest item from an Array of numbers.
+#Do this by using a for loop;
+#if successful, modify the function to make use an Array method that is able to accomplish
+# the same thing with a single method.
--- 8.rb
+++ 8.rb
@@ -1,11 +1,3 @@
 
-# Desired output: 0, 1, 3, 7, 5
-for my_array.each_with_index do |x|
-    if (x%2 ==0) then
-      my_array.push(x.to_i)
-    end
-end
-
-result = my_array[0, 2, 1, 9, 3, 6, 7, 2, 5]
-puts result
-end
+my_array = [0, 2, 1, 9, 3, 6, 7, 2, 5]
+my_array.each_with_index { |a, x| puts a if x % 2 == 0 }
